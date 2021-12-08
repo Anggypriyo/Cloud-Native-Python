f379236213e41d5eaf1ce82d5efd63a907f13790
Anggypriyo <anggypriyo@gmail.com>
2021-12-08 11:03:55 +0700

    Padoru Padoru

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import abort
+from flask import make_response, url_for
+from time import gmtime, strftime
+from flask import render_template, redirect, session
 import json
 import sqlite3
+import datetime
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/addname')
+def addname():
+    sumSessionCounter()
+    if request.args.get('yourname'):
+       session['name'] = request.args.get('yourname')
+       # and then redirect the user to the main page
+       return redirect(url_for('main'))
+    else:
+       return render_template('addname.html', session=session)
+
+def sumSessionCounter():
+  try:
+    session['counter'] += 1
+  except KeyError:
+    session['counter'] = 1
+
+@app.route('/clear')
+def clearsession():
+    # Clear the session
+    session.clear()
+    # Redirect the user to the main page
+    return redirect(url_for('main'))
+
+@app.route('/adduser')
+def adduser():
+    return render_template('adduser.html')
+
+@app.route('/addtweets')
+def addtweetjs():
+    return render_template('addtweets.html')
 
 def list_users():
  conn = sqlite3.connect('mydb.db')
@@ -23,6 +65,53 @@ def list_users():
  conn.close()
  return jsonify({'user_list': api_list})
 
+def list_user(user_id):
+ conn = sqlite3.connect('mydb.db')
+ print ("Opened database successfully");
+ api_list=[]
+ cursor=conn.cursor()
+ cursor.execute("SELECT * from users where id=?", (user_id,))
+ data = cursor.fetchall()
+ if len(data) == 1:
+  user = {}
+  user['username'] = data[0][0]
+  user['email'] = data[0][1]
+  user['password'] = data[0][2]
+  user['name'] = data[0][3]
+  user['id'] = data[0][4]
+ else:
+  user = {}
+  user['error'] = "Resource not found!"
+ conn.close()
+ return jsonify(user)
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'emailid' in request.json or not 'full_name' in request.json or not 'password' in request.json:
+     abort(400)
+    user = {
+        'username': request.json['username'],
+        'emailid': request.json['emailid'],
+        'full_name': request.json.get('full_name',""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database Successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM users where username=? or emailid=?", (new_user['username'], new_user['emailid']))
+    data = cursor.fetchall()
+    if len (data) != 0:
+       abort(409)
+    else:
+       cursor.execute("INSERT INTO users (username,emailid,password,full_name) values(?,?,?,?)", (new_user['username'],new_user['emailid'],new_user['password'],new_user['full_name']))
+       conn.commit()
+       return "Success"
+    conn.close()
+    return jsonify(a_dict)
 
 @app.route("/api/v1/info")
 def home_index():
@@ -40,11 +129,157 @@ def home_index():
  conn.close()
  return jsonify({'version_list': api_list}), 200
 
+@app.route('/api/v1/users', methods=['DELETE'])
+def delete_user():
+    if not request.json or not 'username' in request.json:
+       abort(400)
+    user=request.json['username']
+    return jsonify({'status': del_user(user)}), 200
+    
+def del_user(del_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Openend database successfully");
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users where username=?", (del_user,))
+    data = cursor.fetchall()
+    print ("Data", data)
+    if len (data) == 0:
+       abord(404)
+    else:
+       cursor.execute("DELETE FROm users WHERE username==?", (del_user,))
+    conn.commit()
+    return "Success"
+    
+@app.route('/api/v1/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = {}
+    if not request.json:
+       abort(400)
+    user['id'] = user_id
+    key_list = request.json.keys()
+    for i in key_list:
+       user[i] = request.json[i]
+    print (user)
+    return jsonify({'status': upd_user(user)}), 200
+
+def upd_user(user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id=? ", (user['id'],))
+    data = cursor.fetchall()
+    if len (data) == 0:
+       abort(404)
+    else:
+       key_list=user.keys()
+       for i in key_list:
+           if i != "id":
+              print (user, i)
+              # cursor.execute("UPDATE users set {0}=? where id=? ", (i, user[i], user['id'])
+              cursor.execute("""UPDATE users SET {0} = ? WHERE id = ?""".format(i), (user[i], user['id']))
+              conn.commit()
+    return "Success"
+
+@app.route('/api/v2/tweets', methods=['GET'])
+def get_tweets():
+    return list_tweets()
+
+def list_tweets():
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT username, body, tweet_time, id from tweets")
+    data = cursor.fetchall()
+    print (data)
+    print (len(data))
+    if len(data) == 0:
+        return jsonify({'tweets_list': api_list})
+    else:
+        for row in data:
+            tweets = {}
+
+            tweets['tweetedby'] = row[0]
+            tweets['body'] = row[1]
+            tweets['timestamp'] = row[2]
+            tweets['id'] = row[3]
+
+            print (tweets)
+            api_list.append(tweets)
+
+    conn.close()
+    print (api_list)
+    return jsonify({'tweets_list': api_list})
+
+@app.route('/api/v2/tweets', methods=['POST'])
+def add_tweets():
+    user_tweet = {}
+    if not request.json or not 'username' in request.json or not 'body' in request.json:
+        abort(400)
+    user_tweet['username'] = request.json['username']
+    user_tweet['body'] = request.json['body']
+    user_tweet['created_at']=strftime("%Y-%m-%dT%H:%M:%SZ", gmtime())
+    print (user_tweet)
+    return jsonify({'status': add_tweet(user_tweet)}), 200
+
+def add_tweet(new_tweets):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database Successfully");
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users where username=? ", (new_tweets['username'],))
+    data = cursor.fetchall()
+    if len (data) == 0:
+       abort(404)
+    else:
+       cursor.execute("INSERT INTO tweets (username, body, tweet_time) values(?,?,?)",(new_tweets['username'],new_tweets['body'], new_tweets['created_at']))
+    conn.commit()
+    return "Success"
+
+@app.route('/api/v2/tweets/<int:id>', methods=['GET'])
+def get_tweet(id):
+    return list_tweet(id)
+
+def list_tweet(user_id):
+    print (user_id)
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database Successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM tweets where id=? ", (user_id,))
+    data = cursor.fetchall()
+    print (data)
+    if len(data) == 0:
+       abort(404)
+    else:
+       user={}
+       user['id'] = data[0][0]
+       user['username']= data[0][1]
+       user['body'] = data[0][2]
+       user['tweet_time']= data[0][3]
+    conn.close()
+    return jsonify(user)
+
 
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
   return list_users()
 
+@app.route("/api/v1/users/<int:user_id>", methods=['GET'])
+def get_user(user_id):
+  return list_user(user_id)
+
+@app.errorhandler(400)
+def invalid_request(error):
+     return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return make_response(jsonify({'error':'Resource not found!'}), 404)
+
+@app.errorhandler(409)
+def user_found(error):
+    return make_response(jsonify({'error': 'Conflict! Record exist'}), 409)
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port=5000, debug=True)
+
